5b94a17d63db8fc46e60adefdeaddc159a54d3b1
Scrouly <fevanwood@yahoo.com>
2023-04-06 17:22:27 +0300

    Получение всех записей из бд и добавление их в эластик( вместо чтения из файла)

---
+++ utils/database/database_get_all_data.py
@@ -0,0 +1,29 @@
+from utils.database.database_connection import create_session
+from utils.database.database_models import Document
+import json
+
+
+def database_get_all(session):
+    """"Возвращает все записи из бд"""
+    try:
+        with session() as session:
+            select_query = session.query(Document.id, Document.text)
+            result = select_query.all()
+            print(f'Number of returned documents: {len(result)}')
+            result_list = [({'id': post.id, 'text': post.text}) for post in result]
+
+    except Exception as er:
+        print(f'DB|Error:{er}')
+
+    for document in result_list:
+        yield '{ "index" : { "_index" : "%s", "_type" : "%s", "_id" : "%s"}}' % (
+            "documents", "document", document["id"])
+
+        yield json.dumps(document, default=int)
+
+
+if __name__ == "__main__":
+    session = create_session()
+    print(database_get_all(session))
+    for i in database_get_all(session):
+       print(i)
--- utils/elastic/elastic_entry.py
+++ utils/elastic/elastic_entry.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import json
 
+from utils.database.database_connection import create_session
+from utils.database.database_get_all_data import database_get_all
 from utils.elastic.elastic_connection import connect_elasticsearch
 
 
 def data_to_elastic(data_file, id=0):
     """Конвертация данных в формат elastic'а"""
-    for record in data_file.to_dict(orient="records"):
+
+    data = pd.read_csv(data_file, usecols=[0])
+    data["id"] = data.index + 1
+
+    for record in data.to_dict(orient="records"):
         id += 1
         yield '{ "index" : { "_index" : "%s", "_type" : "%s", "_id" : "%s"}}' % (
             "documents", "document", id)
@@ -14,11 +20,9 @@ def data_to_elastic(data_file, id=0):
         yield json.dumps(record, default=int)
 
 
-def elastic_entry(es_object, data_file):
+def elastic_entry(es_object, data):
     """Добавление данных в Elastic"""
     # Cчитывает колонку text из csv и добавляет колонку id
-    df = pd.read_csv(data_file, usecols=[0])
-    df["id"] = df.index + 1
 
     # Поключение к elastic
     if es_object:
@@ -27,7 +31,7 @@ def elastic_entry(es_object, data_file):
             es_object.indices.delete(index='documents')
         es_object.indices.create(index='documents')
         # Запись данных
-        data_recording = es_object.bulk(data_to_elastic(df))
+        data_recording = es_object.bulk(data)
         if data_recording["errors"]:
             print("Failed to write data to elastic")
         else:
@@ -37,5 +41,8 @@ def elastic_entry(es_object, data_file):
 
 if __name__ == "__main__":
     file_name = "../../data/posts.csv"
+    # data = data_to_elastic(file_name)
+    session = create_session()
+    data = database_get_all(session)
     es = connect_elasticsearch()
-    elastic_entry(es, file_name)
+    elastic_entry(es, data)
